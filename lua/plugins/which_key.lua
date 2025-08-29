ec1969cd8f031973a20fb1e8323efd1fa094f178
meng <1792766874@qq.com>
2025-08-29 10:56:18 +0800

    feat: 添加which_key插件

---
+++ lua/plugins/which_key.lua
@@ -0,0 +1,19 @@
+local M = {
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+}
+
+return M
