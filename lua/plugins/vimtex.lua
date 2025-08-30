2b0954be767cd55010f751041bd9e00cec5cd5ce
meng <1792766874@qq.com>
2025-08-30 21:07:38 +0800

    fix: 修改latex的pdf查看器为sioyek

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,8 +3,21 @@ local M = {
 		"lervag/vimtex",
 		lazy = false,
 		init = function()
-			vim.g.vimtex_view_general_viewer = "SumatraPDF.exe"
-      vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+			vim.g.vimtex_view_method = "sioyek"
+			vim.g.vimtex_view_sioyek_exe = "sioyek.exe"
+
+			vim.g.vimtex_quickfix_ignore_filters = {
+				"Underfull",
+				"Overfull",
+				"specifier changed to",
+				"Token not allowed in a PDF string",
+				"LaTeX Warning: Float too large for page",
+				"contains only floats",
+				"LaTeX Warning: Citation",
+				'Missing "author" in',
+				"LaTeX Font Warning:",
+				'Package option "final" no longer has any effect with minted v3+',
+			}
 		end,
 	},
 }
