6b32673f83cbe460df931e956f77bb3b8b062b71
meng <1792766874@qq.com>
2026-01-05 18:49:49 +0800

    fix：删除多余的插件启动方式

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,29 +2,23 @@ local M = {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		event = { "BufReadPost", "BufNewFile" },
-		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
 		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"lua",
-					"luadoc",
-					"printf",
-					"vim",
-					"vimdoc",
-
-					"rust",
-					"cpp",
-				},
-
-				highlight = {
-					enable = true,
-					use_languagetree = true,
-				},
-
-				indent = { enable = true },
-			})
-		end,
+		opts = {
+			ensure_installed = {
+				"lua",
+				"luadoc",
+				"printf",
+				"vim",
+				"vimdoc",
+				"rust",
+				"cpp",
+			},
+			highlight = {
+				enable = true,
+				use_languagetree = true,
+			},
+			indent = { enable = true },
+		},
 	},
 
 	{
